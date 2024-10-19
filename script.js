98d76d0fac952179daf6ef08e1fd835d92623a9d
sbp <imaboylol8@gmail.com>
2024-10-19 18:27:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.querySelectorAll('.scroll-link').forEach(link => {
     link.addEventListener('click', function(e) {
-        e.preventDefault(); // Prevent default anchor behavior
-        const targetId = this.getAttribute('href'); // Get the href attribute
-        const targetSection = document.querySelector(targetId); // Find the target section
+        e.preventDefault(); 
+        const targetId = this.getAttribute('href'); 
+        const targetSection = document.querySelector(targetId); 
 
-        // Scroll smoothly to the target section
+
         targetSection.scrollIntoView({ behavior: 'smooth' });
 
-        // Update the URL without the hash
-        history.pushState(null, null, 'index.html'); // Change this to your actual file name if different
+
+        history.pushState(null, null, 'https://2validck.github.io/SBPlug.github.io/'); 
     });
 });
 
 // Keep track of the current song index
-// Keep track of the current song index
+
 let currentSongIndex = 0;
 
-// List of songs in the playlist (updated to include only 7 songs)
+// List of songs in the playlist 
 const songs = [
     'song1.mp3',
     'song2.mp3',
